bcceddb860a7abfa64592f2f296a151171857e8c
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-04-26 14:27:32 +0700

    revisi pada while

--- 098_singlelinkedlist/098_singlelinkedlist.cpp
+++ 098_singlelinkedlist/098_singlelinkedlist.cpp
@@ -21,23 +21,34 @@ void addNode() {
 	nodeBaru->name = nama;
 
 	if (START == NULL || nim <= START->noMhs) {
-	{
+		{
 			cout << "NIM sudah ada" << endl;
 			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
 	}
 
-	nodeBaru->next = START;
-	START = nodeBaru;
-	return;
-}
+	Node* previous = START;
+	Node* current = START;
 
-Node* previous = START;
-Node* current = START;
-
-while ((current != NULL) && (nim >= current->noMhs))
-{
-	if (nim == current->noMhs)
+	while ((current != NULL) && (nim >= current->noMhs))
 	{
-
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
 	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;	
 }
+
+
+
+
